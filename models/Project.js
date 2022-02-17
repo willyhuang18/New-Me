0a65bd9807fb65f1bbde731837c8daa635259585
wiily <willyhuang18@gmail.com>
2022-02-16 22:30:29 -0800

    change tier

--- models/Project.js
+++ models/Project.js
@@ -23,13 +23,6 @@ Project.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
   },
   {
     sequelize,
