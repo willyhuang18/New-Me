1b7338808d95c3d99faa1d833cf0908035293245
wiily <willyhuang18@gmail.com>
2022-02-16 23:03:16 -0800

    updating content for project

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,14 +4,7 @@ const { Project, User } = require('../models');
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    const projectData = await Project.findAll({});
 
     // Serialize data so the template can read it
     const projects = projectData.map((project) => project.get({ plain: true }));
