a09a4a03ef8456dc7c7729643ae02c0a13847306
wiily <willyhuang18@gmail.com>
2022-02-16 21:44:02 -0800

    change tier

--- models/index.js
+++ models/index.js
@@ -1,46 +1,13 @@
 const User = require('./User');
 const Project = require('./Project');
-const Skill = require('./Skills');
-const Employment = require('./Employment')
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-User.hasMany(Skill, {
-  foreignKey: 'user_id'
-})
-
-User.hasMany(Employment, {
-  foreignKey: 'user_id'
-})
-
 Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Project.belongsTo(Skill, {
-  foreignKey: 'skill_id'
-});
-
-
-Skill.belongsTo(User, {
-  foreignKey: 'user_id'
-})
-
-Skill.belongsTo(Employment, {
-  foreignKey: 'employment_id'
-})
-
-Employment.belongsTo(User, {
-  foreignKey: 'user_id'
-})
-
-Employment.hasMany(Skill, {
-  foreignKey: 'employment_id'
-})
-
-
-
-module.exports = { User, Project, Employment, Skill};
+module.exports = { User, Project };
